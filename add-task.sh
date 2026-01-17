178e5d895b0a1a089f11dbd2a0e280bb5240116c
f0ld <me@f0ld.space>
2026-01-17 18:16:12 +0000

    faster task adding

--- add-task.sh
+++ add-task.sh
@@ -5,9 +5,9 @@ JOPLIN="/home/f0ld/.npm-global/bin/joplin"
 title=$(echo "" | wofi --dmenu --prompt "Todo name")
 [ -z "$title" ] && exit 0
 
-$JOPLIN mktodo "$title" || exit 1
-
 extra=$(echo "" | wofi --dmenu --prompt "Extra?")
+
+$JOPLIN mktodo "$title" || exit 1
 [ -n "$extra" ] && $JOPLIN set "$title" body "$extra"
 
 notify-send "Todo added" "$title"
